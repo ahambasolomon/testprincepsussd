069e5926cae7ee68cdce1e71d103e210e7e77e4e
Ahamba Solomon <ahamba.solomon@princepsfinance.com>
2021-02-15 15:05:05 +0100

    sdoskddgfd  sds sds

--- index.php
+++ index.php
@@ -4,20 +4,23 @@ $sessionId   = $_POST["sessionId"];
 $serviceCode = $_POST["serviceCode"];  
 $text = $_POST["text"];
 $phone = $_POST["phoneNumber"];
+$amount;
 //This is the first menu screen
 if ( $text == "" ) {
-$response  = "CON Hi welcome to credit wallet quick loan application.  \n";
-$response .= "1. Enter 1 to continue ".$phone;
-
+    $response  = "CON Hi welcome to credit wallet ussd potal application.  \n";
+    $response .= "1. Enter 1 to apple for loan \n";
+    $response .= "2. Enter 2 for loan liquidation \n";
 }
 // Menu for a user who selects '1' from the first menu
 // Will be brought to this second menu screen
 else if ($text == "1") {
-$response  = "CON  Pick a table for reservation below \n";
-$response .= "1. Table for 2 \n";
-$response .= "2. Table for 4 \n";
-$response .= "3. Table for 6 \n";
-$response .= "4. Table for 8 \n";
+    $response  = "CON Enter amount \n";
+    $amount = $text;
+    $response .= $amount;
+// $response .= "1. Table for 2 \n";
+// $response .= "2. Table for 4 \n";
+// $response .= "3. Table for 6 \n";
+// $response .= "4. Table for 8 \n";
 }
 //Menu for a user who selects '1' from the second menu above
 // Will be brought to this third menu screen
