6490ebea1b1cc7eb1b51a177100976c0a160bb85
Ahamba Solomon <ahamba.solomon@princepsfinance.com>
2021-02-15 20:15:37 +0100

    sdsdsdskdsj sdis

--- index.php
+++ index.php
@@ -114,7 +114,7 @@ else if (checkconfirmation($text)) {
     $response = "CON Enter 00 key to Confirm. \n";
     $response .= " Amount: ".getdata($text)[1]." \n";
     $response .= " Duration: ".getdata($text)[2]." Months \n";
-    $response .= " Ippis Number: ".empty(getdata($text)[4]) ? 'Not applicable' : getdata($text)[4]."\n";
+    $response .= " Ippis Number: ".getdata($text)[4] == ' ' ? 'Not applicable'."\n" : getdata($text)[4]."\n";
 }
 
 
